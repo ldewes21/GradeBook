35db29a0cbac74f276fefae9c98622194b411026
ldewes21 <dewes.leandro21@gmail.com>
2021-02-07 16:58:49 -0300

    -all

---
+++ GradeBook.h
@@ -0,0 +1,8 @@
+/* GradeBook.h
+Definições de classes e funções da biblioteca GradeBook.  
+Tenho como filosofia que um código fonte tenha que ser entendido por um leigo, 
+alguém que nunca programou na vida, então meus códigos terão comentários sempre 
+que eu considerar necessário para o entendimento dos leigos.
+*/
+ 
+
